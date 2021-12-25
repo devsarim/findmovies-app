6c9d2bfe5e939a49f00d900820dfb7174c9c5d7e
Sarim <55910649+sarimrb@users.noreply.github.com>
2021-12-26 02:05:39 +0500

    pretty much done

--- main.js
+++ main.js
@@ -1,192 +1,266 @@
+const registerPagination = () => {
+  document.querySelectorAll(".pages-container").forEach((pagesContainer) => {
+    let rootContainer = pagesContainer.parentElement;
+    let pageControls = rootContainer.children[1];
+
+    let prevPage = pageControls.children[0];
+    let currentPageIndicator = pageControls.children[1];
+    let nextPage = pageControls.children[2];
+
+    let pages = pagesContainer.children;
+    let currentPage = pages[0];
+    let currentPageNum = 1;
+
+    let focused = false;
+
+    function goToNextPage() {
+      if (!currentPage) {
+        return;
+      }
+
+      if (currentPage.nextElementSibling) {
+        currentPage.classList.remove("current");
+        currentPage = currentPage.nextElementSibling;
+        currentPage.classList.add("current");
+
+        currentPageNum++;
+        currentPageIndicator.textContent = currentPageNum;
+      } else {
+        currentPage.classList.remove("current");
+        currentPage = pages[0];
+        currentPage.classList.add("current");
+
+        currentPageNum = 1;
+        currentPageIndicator.textContent = currentPageNum;
+      }
+
+      pagesContainer.style.height = currentPage.clientHeight + "px";
+    }
+
+    function goToPrevPage() {
+      if (!currentPage) {
+        return;
+      }
+
+      if (currentPage.previousElementSibling) {
+        currentPage.classList.remove("current");
+        currentPage = currentPage.previousElementSibling;
+        currentPage.classList.add("current");
+
+        currentPageNum--;
+        currentPageIndicator.textContent = currentPageNum;
+      } else {
+        currentPage.classList.remove("current");
+        currentPage = pages[pages.length - 1];
+        currentPage.classList.add("current");
+
+        currentPageNum = pages.length;
+        currentPageIndicator.textContent = currentPageNum;
+      }
+
+      pagesContainer.style.height = currentPage.clientHeight + "px";
+    }
+
+    nextPage.addEventListener("click", goToNextPage);
+    prevPage.addEventListener("click", goToPrevPage);
+
+    rootContainer.addEventListener("mouseover", () => {
+      focused = true;
+    });
+
+    rootContainer.addEventListener("mouseout", () => {
+      focused = false;
+    });
+
+    window.addEventListener("keydown", (e) => {
+      if (focused) {
+        if (e.key === "ArrowRight") {
+          goToNextPage();
+        } else if (e.key === "ArrowLeft") {
+          goToPrevPage();
+        }
+      }
+    });
+
+    // not really necessary
+    window.addEventListener("resize", () => {
+      if (!currentPage) {
+        return;
+      }
+
+      pagesContainer.style.height = currentPage.clientHeight + "px";
+    });
+
+    if (currentPage) {
+      console.log("set initial " + currentPage.clientHeight);
+      pagesContainer.style.height = currentPage.clientHeight + "px";
+      currentPageIndicator.textContent = 1;
+    }
+  });
+};
+
 window.addEventListener("load", () => {
   console.info("DOM Loaded");
 
-  // const MAX_OVERVIEW_WORDS = 35;
-  // const GENRES = [
-  //   { id: 28, name: "Action" },
-  //   { id: 12, name: "Adventure" },
-  //   { id: 16, name: "Animation" },
-  //   { id: 35, name: "Comedy" },
-  //   { id: 80, name: "Crime" },
-  //   { id: 99, name: "Documentary" },
-  //   { id: 18, name: "Drama" },
-  //   { id: 10751, name: "Family" },
-  //   { id: 14, name: "Fantasy" },
-  //   { id: 36, name: "History" },
-  //   { id: 27, name: "Horror" },
-  //   { id: 10402, name: "Music" },
-  //   { id: 9648, name: "Mystery" },
-  //   { id: 10749, name: "Romance" },
-  //   { id: 878, name: "Science Fiction" },
-  //   { id: 10770, name: "TV Movie" },
-  //   { id: 53, name: "Thriller" },
-  //   { id: 10752, name: "War" },
-  //   { id: 37, name: "Western" },
-  // ];
+  const GENRES = [
+    { id: 28, name: "Action" },
+    { id: 12, name: "Adventure" },
+    { id: 16, name: "Animation" },
+    { id: 35, name: "Comedy" },
+    { id: 80, name: "Crime" },
+    { id: 99, name: "Documentary" },
+    { id: 18, name: "Drama" },
+    { id: 10751, name: "Family" },
+    { id: 14, name: "Fantasy" },
+    { id: 36, name: "History" },
+    { id: 27, name: "Horror" },
+    { id: 10402, name: "Music" },
+    { id: 9648, name: "Mystery" },
+    { id: 10749, name: "Romance" },
+    { id: 878, name: "Science Fiction" },
+    { id: 10770, name: "TV Movie" },
+    { id: 53, name: "Thriller" },
+    { id: 10752, name: "War" },
+    { id: 37, name: "Western" },
+  ];
 
-  // const resultsGrid = document.querySelector(".results-grid");
-  // const searchBar = document.querySelector("#search-bar");
+  function sliceIntoChunks(arr, chunkSize) {
+    const res = [];
+    for (let i = 0; i < arr.length; i += chunkSize) {
+      const chunk = arr.slice(i, i + chunkSize);
+      res.push(chunk);
+    }
+    return res;
+  }
 
-  // let lastQuery = "";
+  const searchResultsSection = document.querySelector(
+    "#search-results-section"
+  );
 
-  // function getGenreFromId(id) {
-  //   for (let index = 0; index < GENRES.length; index++) {
-  //     const object = GENRES[index];
-  //     if (object.id === id) {
-  //       return object.name;
-  //     }
-  //   }
-  // }
+  const renderSearchResults = (query) => {
+    if (query === "") {
+      searchResultsSection.style.display = "none";
+      return;
+    }
 
-  // async function renderTrendingMovies() {
-  //   resultsGrid.innerHTML = "";
+    searchResultsSection.style.display = "initial";
+    let pagesContainer = document.querySelector("#search-results");
+    pagesContainer.innerHTML = "";
 
-  //   let res = await fetch(
-  //     "https://findmovies-proxy.glitch.me/api/trendingmovies/today"
-  //   );
+    fetch(`https://findmovies-proxy.glitch.me/api/movies?query=${query}`).then(
+      (res) => {
+        res.json().then((data) => {
+          let groups = sliceIntoChunks(data.results, 4);
 
-  //   res.json().then((data) => {
-  //     data.results.forEach((result) => {
-  //       let overview = result.overview;
-  //       let split = overview.split(" ");
-  //       if (split.length > MAX_OVERVIEW_WORDS) {
-  //         overview = "";
-  //         for (let index = 0; index < MAX_OVERVIEW_WORDS; index++) {
-  //           overview += split[index] + " ";
-  //         }
-  //         overview += "...";
-  //       }
+          groups.forEach((group, index) => {
+            pagesContainer.innerHTML += `
+            <div class="page results-grid ${index === 0 ? "current" : ""}">
+  
+            </div>
+  
+          `;
 
-  //       let genresList = "";
-  //       result.genre_ids.forEach((genreId) => {
-  //         genresList += `<li><a href="https://www.themoviedb.org/genre/${genreId}/movie" class="movie-genre">${getGenreFromId(
-  //           genreId
-  //         )}</a></li>`;
-  //       });
+            let curPage = pagesContainer.children[index];
+            group.forEach((item) => {
+              curPage.innerHTML += `
+            <div class="result-card">
+            <img class="result-poster" src="https://image.tmdb.org/t/p/original${item.poster_path}" alt="Movie Poster" />
+            <h4 class="result-title text-white">
+            ${item.title}
+            </h4>
+            </div>
+            `;
+            });
+          });
 
-  //       resultsGrid.innerHTML += `
-  //           <div class="card">
-  //           <div class="flip-card-container">
-  //           <div class="flip-card-inner">
-  //           <img src="https://image.tmdb.org/t/p/original${
-  //             result.poster_path
-  //           }" alt="movie poster" class="movie-poster" />
-  //           <div class="movie-hover-info">
-  //           <p class="movie-plot">
-  //           ${overview}
-  //           </p>
-  //           <ul class="movie-genres">
-  //           ${genresList}
-  //           </ul>
-  //           </div>
-  //           </div>
-  //           </div>
-  //           <section class="movie-info-container">
-  //           <h4 class="movie-title">${result.title}</h4>
-  //           <div class="movie-info">
-  //           <p class="text-secondary release-date">${
-  //             result.release_date.split("-")[0]
-  //           }</p>
-  //           <p class="text-secondary movie-rating" title="${
-  //             result.vote_count
-  //           } votes">
-  //           <i class="fas fa-star" style="color: #d3c43a"></i>&nbsp;${
-  //             result.vote_average
-  //           }
-  //           </p>
-  //           </div>
-  //           </section>
-  //           </div>
-  //           `;
-  //     });
-  //   });
-  // }
+          registerPagination();
+        });
+      }
+    );
+  };
 
-  // async function searchAndRender(query) {
-  //   resultsGrid.innerHTML = "";
+  const renderTrendingMovies = () => {
+    let pagesContainer = document.querySelector("#trending-movies");
+    pagesContainer.innerHTML = "";
 
-  //   let res = await fetch(
-  //     `https://findmovies-proxy.glitch.me/api/movies/?query=${query}`
-  //   );
-  //   res.json().then((data) => {
-  //     data.results.forEach((result) => {
-  //       let overview = result.overview;
-  //       let split = overview.split(" ");
-  //       if (split.length > MAX_OVERVIEW_WORDS) {
-  //         overview = "";
-  //         for (let index = 0; index < MAX_OVERVIEW_WORDS; index++) {
-  //           overview += split[index] + " ";
-  //         }
-  //         overview += "...";
-  //       }
+    fetch("https://findmovies-proxy.glitch.me/api/trendingmovies/today").then(
+      (res) => {
+        res.json().then((data) => {
+          let groups = sliceIntoChunks(data.results, 4);
 
-  //       let genresList = "";
-  //       result.genre_ids.forEach((genreId) => {
-  //         genresList += `<li><a href="https://www.themoviedb.org/genre/${genreId}/movie" class="movie-genre">${getGenreFromId(
-  //           genreId
-  //         )}</a></li>`;
-  //       });
+          groups.forEach((group, index) => {
+            pagesContainer.innerHTML += `
+            <div class="page results-grid ${index === 0 ? "current" : ""}">
+  
+            </div>
+  
+          `;
 
-  //       resultsGrid.innerHTML += `
-  //           <div class="card">
-  //           <div class="flip-card-container">
-  //           <div class="flip-card-inner">
-  //           <img src="https://image.tmdb.org/t/p/original${
-  //             result.poster_path
-  //           }" alt="movie poster" class="movie-poster" />
-  //           <div class="movie-hover-info">
-  //           <p class="movie-plot">
-  //           ${overview}
-  //           </p>
-  //           <ul class="movie-genres">
-  //           ${genresList}
-  //           </ul>
-  //           </div>
-  //           </div>
-  //           </div>
-  //           <section class="movie-info-container">
-  //           <h4 class="movie-title">${result.title}</h4>
-  //           <div class="movie-info">
-  //           <p class="text-secondary release-date">${
-  //             result.release_date.split("-")[0]
-  //           }</p>
-  //           <p class="text-secondary movie-rating" title="${
-  //             result.vote_count
-  //           } votes">
-  //           <i class="fas fa-star" style="color: #d3c43a"></i>&nbsp;${
-  //             result.vote_average
-  //           }
-  //           </p>
-  //           </div>
-  //           </section>
-  //           </div>
-  //           `;
-  //     });
-  //   });
-  // }
+            let curPage = pagesContainer.children[index];
+            group.forEach((item) => {
+              curPage.innerHTML += `
+            <div class="result-card">
+            <img class="result-poster" src="https://image.tmdb.org/t/p/original${item.poster_path}" alt="Movie Poster" />
+            <h4 class="result-title text-white">
+            ${item.title}
+            </h4>
+            </div>
+            `;
+            });
+          });
 
-  // document.querySelector("[type=submit]").addEventListener("click", (e) => {
-  //   e.preventDefault();
+          registerPagination();
+        });
+      }
+    );
+  };
 
-  //   let query = searchBar.value;
-  //   if (query === "") {
-  //     document.querySelector("#subheading").textContent = "Trending";
-  //     renderTrendingMovies();
-  //     return;
-  //   }
+  const renderTrendingShows = () => {
+    let pagesContainer = document.querySelector("#trending-shows");
+    pagesContainer.innerHTML = "";
 
-  //   if (query === lastQuery) {
-  //     return;
-  //   }
+    fetch("https://findmovies-proxy.glitch.me/api/trendingshows/today").then(
+      (res) => {
+        res.json().then((data) => {
+          let groups = sliceIntoChunks(data.results, 4);
 
-  //   document.querySelector(
-  //     "#subheading"
-  //   ).textContent = `Search results for '${query}'`;
+          groups.forEach((group, index) => {
+            pagesContainer.innerHTML += `
+            <div class="page results-grid ${index === 0 ? "current" : ""}">
+  
+            </div>
+  
+          `;
 
-  //   lastQuery = query;
-  //   searchAndRender(query);
-  // });
+            let curPage = pagesContainer.children[index];
+            group.forEach((item) => {
+              curPage.innerHTML += `
+            <div class="result-card">
+            <img class="result-poster" src="https://image.tmdb.org/t/p/original${item.poster_path}" alt="Movie Poster" />
+            <h4 class="result-title text-white">
+            ${item.name}
+            </h4>
+            </div>
+            `;
+            });
+          });
 
-  // renderTrendingMovies();
+          registerPagination();
+        });
+      }
+    );
+  };
+
+  renderTrendingMovies();
+  renderTrendingShows();
+  registerPagination();
+
+  const searchBar = document.querySelector("#search-bar");
+
+  document.querySelector("[type=submit]").addEventListener("click", (e) => {
+    e.preventDefault();
+
+    renderSearchResults(searchBar.value);
+  });
   //https://image.tmdb.org/t/p/w500/kqjL17yufvn9OVLyXYpvtyrFfak.jpg
 });
